03a93d68eb789de076034cf93efe2d5456e7fb1d
mpolu <mp@projectn.co>
2023-04-12 11:51:11 +0530

    Try with Netlify form example

--- pages/index.js
+++ pages/index.js
@@ -13,141 +13,7 @@ export default function Home() {
       <main>
         <Header title="Contact Form" />
         <div class="flex items-center border border-teal-500 rounded-md py-5 px-5 mt-5">
-          <form
-            class="w-full max-w-lg"
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            onSubmit={(e) => {
-              console.log(e);
-            }}
-          >
-            <div class="flex flex-wrap -mx-3 mb-6">
-              <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label
-                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="first-name"
-                >
-                  First Name
-                </label>
-                <input
-                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                  id="first-name"
-                  name="firstname"
-                  type="text"
-                  placeholder="Jane"
-                />
-                <p class="text-red-500 text-xs italic">
-                  Please fill out this field.
-                </p>
-              </div>
-              <div class="w-full md:w-1/2 px-3">
-                <label
-                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="last-name"
-                >
-                  Last Name
-                </label>
-                <input
-                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="last-name"
-                  name="lastname"
-                  type="text"
-                  placeholder="Doe"
-                />
-              </div>
-            </div>
-            <div class="flex flex-wrap -mx-3 mb-6">
-              <div class="w-full px-3">
-                <label
-                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="email"
-                >
-                  Email
-                </label>
-                <input
-                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="email"
-                  type="email"
-                  name="email"
-                />
-                <p class="text-gray-600 text-xs italic">
-                  Make it as long and as crazy as you'd like
-                </p>
-              </div>
-            </div>
-            <div class="flex flex-wrap -mx-3 mb-2">
-              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label
-                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="city"
-                >
-                  City
-                </label>
-                <input
-                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="city"
-                  name="city"
-                  type="text"
-                  placeholder="Albuquerque"
-                />
-              </div>
-              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label
-                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="state"
-                >
-                  State
-                </label>
-                <div class="relative">
-                  <select
-                    class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="state"
-                    name="state"
-                  >
-                    <option>New Mexico</option>
-                    <option>Missouri</option>
-                    <option>Texas</option>
-                  </select>
-                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg
-                      class="fill-current h-4 w-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label
-                  class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="zip"
-                >
-                  Zip
-                </label>
-                <input
-                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="zip"
-                  name="zip"
-                  type="text"
-                  placeholder="90210"
-                />
-              </div>
-            </div>
-            <div class="flex flex-wrap justify-center -mx-3 mb-6 pt-5">
-              <button
-                class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-2 px-6 rounded"
-                type="submit"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-        {/* <div class="flex items-center border border-teal-500 rounded-md py-5 px-5 mt-5">
-          <form name="contact2" method="POST" data-netlify="true">
+          <form name="contact" method="POST" data-netlify="true">
             <p>
               <label>
                 Your Name: <input type="text" name="name" />
@@ -176,7 +42,7 @@ export default function Home() {
               <button type="submit">Send</button>
             </p>
           </form>
-        </div> */}
+        </div>
       </main>
 
       <Footer />
