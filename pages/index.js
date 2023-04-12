55685b2e69d8ff8a36170cd6116e793d30aa7f92
mpolu <mp@projectn.co>
2023-04-12 11:26:27 +0530

    Second form to test

--- pages/index.js
+++ pages/index.js
@@ -143,6 +143,37 @@ export default function Home() {
             </div>
           </form>
         </div>
+        <div class="flex items-center border border-teal-500 rounded-md py-5 px-5 mt-5">
+          <form name="contact2" method="POST" data-netlify="true">
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{" "}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </div>
       </main>
 
       <Footer />
